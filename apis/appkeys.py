7986d3559d832202e3d8fe839378387097cc80bf
Richie Zeng <richzeng@gmail.com>
2012-03-03 15:42:55 -0800

    added more features to dropbox like index checking

--- apis/appkeys.py
+++ apis/appkeys.py
@@ -1,1 +1,2 @@
 DROPBOX = {'key': 'sj2yo0nnqekhpup', 'secret': '47yey792gy6nb58'}
+BOX = {'key': 'ye04mdf6e3scp9nvbybaoine5ypdb0gg', 'secret': ''}
---
+++ apis/box_wrapper.py
@@ -0,0 +1,4 @@
+import appkeys
+
+import os
+import time
