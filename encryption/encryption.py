55436be8476e2988d8a66a4496b7390d711fe3dc
Jian <jianwei1801@gmail.com>
2012-03-03 11:11:25 -0800

    added basic encryption/splitting interface

---
+++ encryption/encryption.py
@@ -0,0 +1,10 @@
+"""
+File Encryption Module
+
+Input: String portion of a file
+Output: Encrypted string portion of the same file
+
+"""
+
+def encrypt(string):
+
---
+++ encryption/splitter.py
@@ -0,0 +1,20 @@
+"""
+File Splitting Module
+
+Input: path/to/file
+Output: encrpyted file string portions
+
+Output String format: file_name+file_index+string_portion
+
+"""
+
+from encryption import encrypt
+
+def open_file(file_path):
+    """Returns a """
+
+def rename_file(file_path):
+    """Renames a file."""
+
+def upload_file(file_path, encrypt=True):
+    """Takes in a file path, encrypts it if required, and outputs a set of split files."""
