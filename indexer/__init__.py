b1f5ca2069cb72272eb721387fc0ac7370841895
Nikita Kitaev <nikitakit@gmail.com>
2012-03-03 11:22:20 -0800

    Indexer framework

---
+++ indexer/__init__.py
@@ -0,0 +1,8 @@
+"""Indexer module
+
+To get the index do "from indexer import index".
+See Index class for methods to call on it
+"""
+
+import indexer
+index = indexer.Index()
---
+++ indexer/indexer.py
@@ -0,0 +1,9 @@
+class Index(object):
+    """An index of mappings between local files and files in the cloud
+    """
+
+    def __init__(self):
+        """Initialize the index
+        """
+        pass
+
